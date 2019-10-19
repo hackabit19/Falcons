b9f5323ebc3e0a3180fccc94c58a67dcf4553d90
amana632 <be10674.17@bitmesra.ac.in>
2019-10-19 21:05:23 +0530

    models_updated

--- BACKEND/main/model.py
+++ BACKEND/main/model.py
@@ -8,7 +8,7 @@ from main import ma
 class User(db.Model):
     __tablename__ = 'user'
     user_id = db.Column(db.String(255), unique=True)
-    user_name = db.Column(db.String(255), unique=True)
+    user_name = db.Column(db.String(255))
     phone = db.Column(db.String(255), primary_key=True)
     wallet_add = db.Column(db.String(255))
     type_s = db.Column(db.String(255))
@@ -29,32 +29,21 @@ user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
 
-class User(db.Model):
-    __tablename__ = 'user'
-    user_id = db.Column(db.String(255), unique=True)
-    user_name = db.Column(db.String(255), unique=True)
+class Slaves(db.Model):
+    __tablename__ = 'slaves'
+    time_stamp = db.Column(db.String(255))
+    data_amount = db.Column(db.String(255))
     phone = db.Column(db.String(255), primary_key=True)
-    pin = db.Column(db.Integer)
-    photo = db.Column(db.String(255))
 
-    def __init__(self, user_id, user_name, name, phone, is_otp_verified, address_id, email, is_email_verified, password, pin, photo):
-        self.user_id = user_id
-        self.user_name = user_name
-        self.name = name
+    def __init__(self, time_stamp, data_amount, phone):
+        self.time_stamp = time_stamp
+        self.data_amount = data_amount
         self.phone = phone
-        self.is_otp_verified = is_otp_verified
-        self.address_id = address_id
-        self.email = email
-        self.is_email_verified = is_email_verified
-        self.password = password
-        self.pin = pin
-        self.photo = photo
 
-
-class UserSchema(ma.Schema):
+class SlaveSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('user_id', 'user_name', 'name', 'phone', 'is_otp_verified', 'address_id', 'email', 'is_email_verified', 'password', 'pin', 'photo')
+        fields = ('time_stamp', 'data_amount', 'phone')
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+slave_schema = SlaveSchema()
+slaves_schema = SlaveSchema(many=True)
