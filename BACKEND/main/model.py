8fdfdbc9dcd4b6d2e2eb4e7374d734957b45137b
amana632 <be10674.17@bitmesra.ac.in>
2019-10-19 21:40:48 +0530

    model updated

--- BACKEND/main/model.py
+++ BACKEND/main/model.py
@@ -51,42 +51,50 @@ class SlaveSchema(ma.Schema):
 slave_schema = SlaveSchema()
 slaves_schema = SlaveSchema(many=True)
 
-class Client_trans(db.Model):
-    __tablename__ = 'slaves'
-    slave_id = = db.Column(db.Integer, primary_key=True)
-    time_stamp = db.Column(db.String(255))
+class ClientTrans(db.Model):
+    __tablename__ = 'clienttrans'
+    clienttrans_id = = db.Column(db.Integer, primary_key=True)
+    time_stamp_start = db.Column(db.String(255))
+    time_stamp_end = db.Column(db.String(255))
     data_amount = db.Column(db.String(255))
+    status = db.Column(db.String(255))
     phone = db.Column(db.String(255))
 
-    def __init__(self, time_stamp, data_amount, phone):
-        self.time_stamp = time_stamp
+    def __init__(self, time_stamp_start, time_stamp_end, data_amount, status, phone):
+        self.time_stamp_start = time_stamp_start
+        self.time_stamp_end = time_stamp_end
         self.data_amount = data_amount
+        self.status = status
         self.phone = phone
 
-class SlaveSchema(ma.Schema):
+class ClientTransSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('slave_id', 'time_stamp', 'data_amount', 'phone')
+        fields = ('clienttrans_id', 'time_stamp_start', 'time_stamp_end', 'data_amount', 'status', 'phone')
 
-slave_schema = SlaveSchema()
-slaves_schema = SlaveSchema(many=True)
+clienttrans_schema = ClientTransSchema()
+clienttranss_schema = ClientTransSchema(many=True)
 
-class Slave_trans(db.Model):
-    __tablename__ = 'slaves'
-    slave_id = = db.Column(db.Integer, primary_key=True)
-    time_stamp = db.Column(db.String(255))
+class SlaveTrans(db.Model):
+    __tablename__ = 'slavetrans'
+    slavetrans_id = = db.Column(db.Integer, primary_key=True)
+    time_stamp_start = db.Column(db.String(255))
+    time_stamp_end = db.Column(db.String(255))
     data_amount = db.Column(db.String(255))
+    status = db.Column(db.String(255))
     phone = db.Column(db.String(255))
 
-    def __init__(self, time_stamp, data_amount, phone):
-        self.time_stamp = time_stamp
+    def __init__(self, time_stamp_start, time_stamp_end, data_amount, status, phone):
+        self.time_stamp_start = time_stamp_start
+        self.time_stamp_end = time_stamp_end
         self.data_amount = data_amount
+        self.status = status
         self.phone = phone
 
-class SlaveSchema(ma.Schema):
+class SlaveTransSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('slave_id', 'time_stamp', 'data_amount', 'phone')
+        fields = ('slavetrans_id', 'time_stamp_start', 'time_stamp_end', 'data_amount', 'status', 'phone')
 
-slave_schema = SlaveSchema()
-slaves_schema = SlaveSchema(many=True)
+slavetrans_schema = SlaveTransSchema()
+slavetranss_schema = SlaveTransSchema(many=True)
